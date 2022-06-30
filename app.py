0ca0bfdd2fd2a9454450a51a03dbc300b4988394
UnknownCaz <unicornprincess522@gmail.com>
2022-06-30 03:01:31 -0400

    finished the remove_by_letters function

--- app.py
+++ app.py
@@ -14,19 +14,18 @@ def n_letter_words(words, length=5):
         if len(word) == length:
             buff.append(word)
     return buff
-#TODO add multiple letter compat
+
 def remove_by_letters(words: list, out_letters, in_letters, print_words=False):
     buff = []
     for word in words:
-        has_out = not any(list(out_letters) in _ for _ in list(word))
-        has_in = any(in_letters in _ for _ in word)
-        if has_out and has_in:
-            buff.append(word)
+        has_out = not any([[i for i in list(out_letters) if i in word] for j in list(word) if j in word])
+        has_in = sorted("".join([j for j in word if j in in_letters])) == sorted(in_letters)
         if print_words:
             print(f"Word: {word}, Out:{has_out}, In:{has_in}")
-    print(buff)
+        if has_out and has_in:
+            buff.append(word)
     return buff
 word_list = n_letter_words(words.words(), length=5)
-word_list = remove_by_letters(word_list,"ur","y",print_words=True)
+word_list = remove_by_letters(word_list,"gmfiestounpicky","arl",print_words=False)
 
-for i in word_list: print(i)
+for i in word_list: print(i, end=" ")
--- test.py
+++ test.py
@@ -1,9 +1,9 @@
-testword = "young"
+testword = "sltricky"
 
 def remove_by_letters(words: list, out_letters, in_letters, print_words=False):
     for word in words:
-        has_out = [[i for i in list(out_letters) if i in word] for j in list(word) if j in word]
-        has_in = any(in_letters in _ for _ in word)
+        has_out = not any([[i for i in list(out_letters) if i in word] for _ in list(word)])
+        has_in = sorted("".join([j for j in word if j in in_letters])) == sorted(in_letters)
         print(f"Word: {word}, Out:{has_out}, In:{has_in}")
 
-remove_by_letters([testword],"uo", "y")
+remove_by_letters([testword],"uo", "il")
